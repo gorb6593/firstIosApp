7e4f66a5ea49913e061a86de419e9b913d16325a
harry <gorb6593@gmail.com>
2025-04-29 23:20:46 +0900

    Initial Commit

---
+++ firstIosApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firstIosApp
+//
+//  Created by 해규 on 4/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello22, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firstIosApp/firstIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstIosAppApp.swift
+//  firstIosApp
+//
+//  Created by 해규 on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct firstIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
